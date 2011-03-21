8a18d0e09af14b983beed24430cb266de207a7b0
saremic <Rakolis@hotmail.com>
2011-03-21 11:50:38 +0200

    Pirdėta projekto exception klasė

---
+++ mifOS/src/Exception/MifOSException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Exception;
+
+/**
+ *
+ * @author Karolis Voicechovskis
+ */
+public class MifOSException extends Exception
+{
+
+    public MifOSException(String message)
+    {
+        super(message);
+    }
+    
+}
